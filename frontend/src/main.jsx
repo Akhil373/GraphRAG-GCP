d8658a6c4247999c38f9876efe388c6c9d64bfe2
Akhil <133588800+Akhil373@users.noreply.github.com>
2025-07-22 20:53:49 +0530

    added analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,15 @@
+import { Analytics } from "@vercel/analytics/next"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './index.css'
 import { ThemeProvider } from './ThemeContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
       <App />
+      <Analytics />
     </ThemeProvider>
   </StrictMode>,
 )
